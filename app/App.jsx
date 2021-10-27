0f4ce0e21598f9e9ba6ccdd45738e7ddc6e6b3e4
Dave Landry <landry.dave@gmail.com>
2021-10-27 17:15:27 -0400

    hooks up Nav and global ProfileSearch

--- app/App.jsx
+++ app/App.jsx
@@ -1,16 +1,38 @@
 import React, {Component} from "react";
+import {withNamespaces} from "react-i18next";
+import {connect} from "react-redux";
+import Nav from "./components/Nav";
+import {ProfileSearch} from "@datawheel/canon-cms";
+import profileSearchConfig from "$app/helpers/search";
 import "./App.css";
 
 class App extends Component {
 
   render() {
+
+    const {searchVisible, t} = this.props;
+
     return (
       <div>
+        <Nav />
         { this.props.children }
+        { searchVisible
+          ? <div
+            className={`global-search ${searchVisible ? "is-visible" : "is-hidden"}`}
+            aria-hidden={!searchVisible}
+            tabIndex={searchVisible ? undefined : -1}>
+              <ProfileSearch
+                {...profileSearchConfig(t)}
+                display="grid"
+                showExamples={true} />
+          </div>
+          : null }
       </div>
     );
   }
 
 }
 
-export default App;
+export default withNamespaces()(connect(state => ({
+  searchVisible: state.searchVisible
+}))(App));
---
+++ app/components/Nav.jsx
@@ -0,0 +1,55 @@
+import React, {Component} from "react";
+import {Link} from "react-router";
+import throttle from "@datawheel/canon-cms/src/utils/throttle";
+import {connect} from "react-redux";
+import {Icon} from "@blueprintjs/core";
+import {toggleSearch} from "$app/actions/search";
+import "./Nav.css";
+
+class Nav extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {scrolled: false};
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    throttle(() => {
+      const screenTop = window.pageYOffset;
+      const newScrolled = screenTop > 100;
+      if (newScrolled !== this.state.scrolled) {
+        this.setState({scrolled: newScrolled});
+      }
+    }, 50);
+  };
+
+  render() {
+
+    const {searchVisible, toggleSearch} = this.props;
+    const {scrolled} = this.state;
+
+    return (
+      <nav className={scrolled ? "background" : ""}>
+        <Link className="logo" to="/">
+          <img src="/images/cfc-logo.png" />
+        </Link>
+        <Icon icon={searchVisible ? "cross" : "search"} size={20} onClick={toggleSearch} />
+      </nav>
+    );
+  }
+
+}
+
+export default connect(state => ({
+  searchVisible: state.searchVisible
+}), dispatch => ({
+  toggleSearch: () => dispatch(toggleSearch())
+}))(Nav);
