2e0b1ffe0bb27d31133a2541e32654487cc79cbb
Dave Landry <landry.dave@gmail.com>
2021-11-01 18:29:58 -0400

    adds initial d3plus text styles

--- app/d3plus.js
+++ app/d3plus.js
@@ -1,12 +1,33 @@
-/**
-  The object exported by this file will be used as a base config for any
-  d3plus-react visualization rendered on the page.
-*/
+import styles from "style.yml";
+
+import {colorDefaults} from "d3plus-color";
+colorDefaults.dark = styles.gris;
+
+const labelStyle = {
+  fontFamily: () => "'Oswald', sans-serif",
+  fontSize: () => 13,
+  fontWeight: () => 300
+};
+
+const axisConfig = {
+  labelConfig: labelStyle,
+  shapeConfig: {
+    labelConfig: labelStyle
+  },
+  titleConfig: {
+    fontFamily: () => "'Oswald', sans-serif",
+    fontSize: () => 15,
+    fontWeight: () => 300
+  }
+};
 
 export default {
+  colorScaleConfig: {
+    axisConfig
+  },
   shapeConfig: {
-    labelConfig: {
-      fontFamily: "Roboto"
-    }
-  }
+    labelConfig: labelStyle
+  },
+  xConfig: axisConfig,
+  yConfig: axisConfig
 };
