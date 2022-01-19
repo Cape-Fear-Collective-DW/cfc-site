fd06454380c63ab019e60bdc8e12949df771331c
Dave Landry <landry.dave@gmail.com>
2022-01-19 17:37:52 -0500

    updates site text titles to Healthy Communities NC

--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -56,13 +56,13 @@ class Home extends Component {
           <Sponsors />
         </div>
         <div className="home-intro">
-          <h2>What is Healthy Cape Fear?</h2>
+          <h2>What is Healthy Communities NC?</h2>
           <div>
             <p>
               The <b>Healthy NC 2030</b> taskforce, led by NCIOM and NCDHHS, brought together experts and leaders from multiple fields to develop a <b>common set of public health indicators and targets for the state over the next decade</b>. The full report, released in January of 2020, established a north star for localities and organizations to mobilize to accomplish North Carolina’s most pressing goals across housing, education, public health, and economic opportunity.
             </p>
             <p>
-              <b>Cape Fear Collective</b> is committed to helping partner organizations set and measure local goals through its expertise in <b>data &amp; analytics</b>. Our <b>Healthy Cape Fear Dashboard</b> enables alignment of community data collection and analysis to the Healthy NC 2030 initiative in order to empower shared hypothesis generation, goal setting, and program measurement.
+              <b>Cape Fear Collective</b> is committed to helping partner organizations set and measure local goals through its expertise in <b>data &amp; analytics</b>. Our <b>Healthy Communities NC Dashboard</b> enables alignment of community data collection and analysis to the Healthy NC 2030 initiative in order to empower shared hypothesis generation, goal setting, and program measurement.
             </p>
           </div>
         </div>
