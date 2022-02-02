32ad1704bae93adf2c98d054d2a2fabff3780303
Dave Landry <landry.dave@gmail.com>
2022-02-02 16:51:43 -0500

    fixes custom SubGrouping section not appearing in SubNav

--- app/cms/sections/SubGrouping.jsx
+++ app/cms/sections/SubGrouping.jsx
@@ -1,17 +1,17 @@
 import React, {Component} from "react";
-import "./IndicatorGroup.css";
+import "./SubGrouping.css";
 
-export default class IndicatorGroup extends Component {
+export default class SubGrouping extends Component {
   render() {
     const {slug, heading, paragraphs, loading, stats} = this.props;
 
     return (
       <div
-        className={`cp-section-inner cp-indicator-group-section-inner cp-${slug}-section-inner ${loading ? "is-loading" : ""}`}
+        className={`cp-section-inner cp-sub-grouping-section-inner cp-${slug}-section-inner ${loading ? "is-loading" : ""}`}
         ref={comp => this.section = comp}
       >
         {/* sidebar */}
-        <div className="cp-section-content cp-indicator-group-section-caption">
+        <div className="cp-section-content cp-sub-grouping-section-caption">
           {stats}
           <div className="cp-paragraph-group">
             {paragraphs}
