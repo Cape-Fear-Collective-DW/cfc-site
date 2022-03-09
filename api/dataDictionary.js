6812dc9a0d60b4ca4663ed3409e6a2a8ff12dee6
Dave Landry <landry.dave@gmail.com>
2022-03-09 13:22:22 -0500

    creates dynamic CSV endpoint

--- api/dataDictionary.js
+++ api/dataDictionary.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2");
+const {Parser} = require("json2csv");
 
 module.exports = function(app) {
 
@@ -9,8 +10,8 @@ module.exports = function(app) {
   const prepData = d => {
     Object.keys(d).forEach(key => {
       if (key.includes("_fips")) {
-        if (key.includes("state")) d[key.replace("_fips", "")] = stateFips[d[key]] || "State Total";
-        if (key.includes("county")) d[key.replace("_fips", "")] = countyFips[d[key]] || "N/A";
+        if (key.includes("state")) d[key.replace("_fips", "")] = stateFips[d[key]] || "N/A";
+        if (key.includes("county")) d[key.replace("_fips", "")] = d[key] === "000" ? "State Total" : countyFips[d[key]] || "N/A";
       }
     });
     return d;
@@ -19,18 +20,36 @@ module.exports = function(app) {
   app.get("/data", async(req, res) => {
 
     connection.query("SELECT tags, attribute_description, tablename, geography, region, vintage, source, notes FROM `data_dictionary` GROUP BY tablename", (err, results) => {
-      res.json(results);
+      return res.json(results);
     });
 
   });
 
-  app.get("/data/:table", async(req, res) => {
+  app.get("/data/:table/:format", async(req, res) => {
 
-    const {table} = req.params;
+    const {table, format} = req.params;
 
-    connection.query(`SELECT * FROM \`${table}\` LIMIT 10`, (err, results) => {
-      res.json(results.map(prepData));
-    });
+    if (format === "csv") {
+
+      connection.query(`SELECT * FROM \`${table}\``, (err, results) => {
+        const data = results.map(prepData);
+        const fields = Object.keys(data[0]);
+        const json2csv = new Parser({fields});
+        const csv = json2csv.parse(data);
+        res.header('Content-Type', 'text/csv');
+        res.attachment(`${table}.csv`);
+        return res.send(csv);
+      });
+
+    }
+    else {
+
+      connection.query(`SELECT * FROM \`${table}\` LIMIT 10`, (err, results) => {
+        const data = results.map(prepData);
+        return res.json(data);
+      });
+
+    }
 
   });
 
