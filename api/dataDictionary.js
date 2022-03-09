486f8c8da3669fd6e9f215ac2115965f5c6ceda7
Dave Landry <landry.dave@gmail.com>
2022-03-09 13:16:07 -0500

    folds in state and county names to data preview

--- api/dataDictionary.js
+++ api/dataDictionary.js
@@ -4,6 +4,18 @@ module.exports = function(app) {
 
   const connection = mysql.createConnection(process.env.CANON_AWS_DB);
 
+  const {countyFips, stateFips} = app.settings.cache;
+
+  const prepData = d => {
+    Object.keys(d).forEach(key => {
+      if (key.includes("_fips")) {
+        if (key.includes("state")) d[key.replace("_fips", "")] = stateFips[d[key]] || "State Total";
+        if (key.includes("county")) d[key.replace("_fips", "")] = countyFips[d[key]] || "N/A";
+      }
+    });
+    return d;
+  };
+
   app.get("/data", async(req, res) => {
 
     connection.query("SELECT tags, attribute_description, tablename, geography, region, vintage, source, notes FROM `data_dictionary` GROUP BY tablename", (err, results) => {
@@ -16,8 +28,8 @@ module.exports = function(app) {
 
     const {table} = req.params;
 
-    connection.query(`SELECT * FROM \`${table}\` LIMIT 100`, (err, results) => {
-      res.json(results);
+    connection.query(`SELECT * FROM \`${table}\` LIMIT 10`, (err, results) => {
+      res.json(results.map(prepData));
     });
 
   });
