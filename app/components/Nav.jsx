1b93aeb495f3ed1ee3af831193762677cded763a
Dave Landry <landry.dave@gmail.com>
2023-05-12 11:31:19 -0400

    adds Profile title to Nav (closes #91)

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Link} from "react-router";
 import throttle from "@datawheel/canon-cms/src/utils/throttle";
+import stripP from "@datawheel/canon-cms/src/utils/formatters/stripP";
 import {connect} from "react-redux";
 import {Icon} from "@blueprintjs/core";
 import {toggleSearch} from "$app/actions/search";
@@ -31,16 +32,26 @@ class Nav extends Component {
     }, 50);
   };
 
+  scrollTop = () => {
+    window.scroll({top: 0, left: 0, behavior: "smooth"});
+  };
+
   render() {
 
-    const {homePage, profilePage, searchVisible, toggleSearch} = this.props;
+    const {homePage, profile, profilePage, searchVisible, toggleSearch} = this.props;
     const {scrolled} = this.state;
+
+    let title = null;
+    if (profile && profile.sections && profile.sections.length) {
+      title = `${stripP(profile.sections[0].title)} ${stripP(profile.sections[0].subtitles[0].subtitle)}`;
+    }
 
     return (
       <nav className={`site-nav ${(!homePage && !profilePage) || scrolled ? "background" : ""}`}>
         <Link className={`logo ${(!homePage || scrolled) ? "visible" : ""}`} to="/">
           <img src="/images/hcnc-logo.svg" />
         </Link>
+        { profilePage && title ? <div onClick={this.scrollTop} className={`nav-profile-title ${scrolled ? "visible" : ""}`}>{title}</div> : null}
         <Icon icon={searchVisible ? "cross" : "search"} size={20} onClick={toggleSearch} />
       </nav>
     );
@@ -52,6 +63,7 @@ export default connect(state => {
   const pathname = state.routing.locationBeforeTransitions ? `/${state.routing.locationBeforeTransitions.pathname}` : state.location.pathname;
   return {
     homePage: pathname === "//" || pathname === "/",
+    profile: state.data.profile,
     profilePage: pathname.includes("profile/"),
     searchVisible: state.searchVisible
   };
